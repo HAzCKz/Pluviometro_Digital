d50e9f465f79efa9b47bf4ddf1616521ccbcadbc
Pedro Augusto <82516407+HAzCKz@users.noreply.github.com>
2025-01-09 11:28:57 -0300

    Add files via upload

---
+++ sensor_reed.ino
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define sensor 13
+#define Led 32
+
+int res;
+int estado;
+int ultimo_estado;
+
+void setup() {
+  pinMode(sensor, INPUT);
+  pinMode(Led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+    estado = digitalRead(sensor);
+
+    if (estado == HIGH)
+    {
+      digitalWrite(Led, HIGH);
+    }
+    else
+    {
+      digitalWrite(Led, LOW);
+    }
+    delay(350);
+
+    contar();
+
+    ultimo_estado = estado;
+
+}
+
+void contar()
+{
+  if (estado != ultimo_estado)
+  {
+    res = res + 1;
+    Serial.println(res);
+    delay(350);
+  }
+  else
+  {
+    Serial.println(res);
+  }
+  
+}
