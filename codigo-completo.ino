d94a71c321f28ef0de0222ebfe9d7f1c9ed20faa
Pedro Augusto <82516407+HAzCKz@users.noreply.github.com>
2025-02-04 13:36:42 -0300

    Update codigo-completo.ino

--- codigo-completo.ino
+++ codigo-completo.ino
@@ -1,1 +1,49 @@
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "id"
+#define BLYNK_TEMPLATE_NAME "DHT11"
+#define BLYNK_AUTH_TOKEN "token"
 
+#include <BlynkSimpleEsp32.h>
+#include <DHT.h>
+#include <stdio.h>
+
+#define DHTYPE DHT11
+#define DHTPIN A14
+
+DHT dht(DHTPIN, DHTYPE);
+
+BlynkTimer timer;
+
+void VirtualWrite()
+{
+  double temp = dht.readTemperature();
+  double umi = dht.readHumidity();
+
+  if(isnan(temp) || isnan(umi))
+  {
+    Serial.println("Falha na leitura do sensor!");
+  }
+  else
+  {
+    Serial.print("Temperatura (C): ");
+    Serial.println(temp);
+    Serial.print("Umidade (%): ");
+    Serial.println(umi);
+  }
+  Blynk.virtualWrite(V0, temp);
+  Blynk.virtualWrite(V5, umi);
+}
+
+void setup() 
+{
+  Serial.begin(9600);
+  dht.begin();
+  Blynk.begin(BLYNK_AUTH_TOKEN, "ssid", "senha", "blynk.cloud", 80);
+  timer.setInterval(1000L, VirtualWrite);
+}
+
+void loop() 
+{
+  Blynk.run();
+  timer.run();
+}
